c797758cb75324cb32bf61ebe061e8d2d2c45092
38369688 <alextsai@Alexs-MacBook-Pro-2.local>
2015-11-04 12:48:20 -0800

    check in

---
+++ src/snakez/Snakez.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snakez;
+
+import environment.ApplicationStarter;
+
+/**
+ *
+ * @author alextsai
+ */
+public class Snakez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ApplicationStartertarter.run("Sanke run", prison());
+    }
+    
+}
